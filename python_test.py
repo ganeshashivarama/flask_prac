3cdb4f8470079b6df6798055993a08ed0cb52b08
Ganesha S <ganesha.s@ericsson.com>
2024-03-06 20:37:24 +0530

    final commit release 1.1.0

--- python_test.py
+++ python_test.py
@@ -1,5 +1,6 @@
 print("Hello brother!")
 print("Welcome to GIT repository!!")
-print("Welcome to Release1.1")
-print("New feature for release 1.1")
+print("Welcome to Release1.1.0")
+print("New feature for release 1.1.0")
 print("Done today!")
+print("Exit..!!")
